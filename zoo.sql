2a874b282565c286b7fd22958015bd2ce68dc6b7
Vaughn Antonyan <vaughnanton@gmail.com>
2017-10-13 13:01:19 -0700

    stage 3 complete

--- zoo.sql
+++ zoo.sql
@@ -108,3 +108,32 @@ Show the population as a percentage of the population of Germany.*/
 --List each continent and the name of country that comes first alphabetically
 8. SELECT continent, name FROM world x WHERE x.name <= ALL (
     SELECT name FROM world y WHERE x.continent=y.continent);
+
+
+
+
+
+--Aggregate Functions
+
+--Show the total population of the world
+1.  SELECT SUM(population) FROM world;
+
+--List all continents, once each
+2. SELECT DISTINCT(continent) FROM world;
+
+--Give the total GDP of Africa
+3. SELECT SUM(GDP) FROM world WHERE continent = 'Africa';
+
+--Show how many countries have area of at least 1000000
+4. SELECT COUNT(name) FROM world WHERE area >= 1000000;
+
+--What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+5. SELECT SUM(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+--For each continent show the continent and number of countries.
+6. SELECT continent, COUNT(name) FROM world
+    WHERE population >= 10000000 GROUP BY continent;
+
+--List the continents that have a total population of at least 100 million.
+7. SELECT continent FROM world GROUP BY continent
+    HAVING SUM(population) >= 10000000;
